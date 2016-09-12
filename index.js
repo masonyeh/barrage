4b3b893e82fbdf73bb534d1af46f6514be0898d3
yew <yew@16care.com>
2016-09-12 14:18:39 +0800

    删除冗余代码

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-javascript:(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})()
     //弹幕
    var Barrage = {
      Socket   :io(), //Socket对象
